5e566130fe05cb3b63052974f31a961b7e8117ca
razil5 <34110362+razil5@users.noreply.github.com>
2024-04-17 21:00:58 +0200

    add mobile

---
+++ mobile.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
+
+let sum = parseInt(num1) + parseInt(num2);
+
+console.log(`The sum of the two numbers is: ${sum}`);
